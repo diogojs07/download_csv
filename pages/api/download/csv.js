b5111d5bec9cd7378df9f6839fb73a26d0c002a2
ViniciusDiLellaBrisken <vinicius.goncalves@brisken.com>
2022-06-02 15:51:19 -0300

    usando outra lib

--- pages/api/download/csv.js
+++ pages/api/download/csv.js
@@ -1,21 +1,24 @@
-import downloadCsv from 'download-csv';
+import { Parser } from 'json2csv';
 
+export const downloadResource = (res, fileName, fields, data) => {
+  const json2csv = new Parser({ fields });
+  const csv = json2csv.parse(data);
+  res.setHeader('Content-Type', 'text/csv');
+  // res.attachament(fileName);
+  return res.send(csv);
+}
 export default function handler(req, res) {
+  const data = [
+    { name: 'test1', score: 1, level: 'Z' },
+    { name: 'test2', score: 2 },
+    { name: 'test3', score: 3 },
+    { name: 'test4', score: 4 },
+    { name: 'test5', score: 5 },
+    { name: 'test6', score: 6 },
+    { name: 'test7', score: 7 },
+  ];
 
-    const data = [
-        { name: 'TEST1', score: 1, level: 'Z' },
-        { name: 'TEST2', score: 2 },
-        { name: 'TEST3', score: 3 },
-        { name: 'TEST4', score: 4 },
-      ];
-       
-      const columns = { name: 'TESTE', score: 'TESTE2' };
+  const fields = [{ label: 'Name', value: 'name' },{ label: 'Score', value: 'score' }]
+  downloadResource(res, 'test',fields,data)
 
-      const filename = 'TEST'
-
-      console.log(data, columns, filename)
-
-    downloadCsv(data, columns, filename);
-
-    res.status(200).json("download")
   }
